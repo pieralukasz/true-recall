93b3b453d875bb478356c3f2901ed4807f2c7af6
Lucas Piera <pieralukasz@gmail.com>
2026-02-20 15:15:04 +0100

    chore: migrate from npm to bun and remove image picker modal

--- src/shared/ui/modals/image-picker/index.ts
+++
@@ -1,5 +0,0 @@
-export { ImageGrid } from "@shared/ui/modals/image-picker/ImageGrid";
-export {
-	ImagePickerBody,
-	type ImagePickerBodyProps,
-} from "@shared/ui/modals/image-picker/ImagePickerBody";
