5342f4cd028161db96942589b8a8f481d8fc0d55
Lucas Piera <pieralukasz@gmail.com>
2026-03-04 09:36:05 +0100

    WIP: session checkpoint

--- src/shared/ui/modals/simple-editor/index.ts
+++
@@ -1,7 +0,0 @@
-export { KeyboardShortcutsHint } from "@shared/ui/modals/simple-editor/KeyboardShortcutsHint";
-export {
-	AddFlashcardsApp,
-	type AddFlashcardsAppProps,
-	type AddFlashcardsResult,
-} from "@shared/ui/modals/simple-editor/AddFlashcardsApp";
-export { NotePickerCombobox } from "@shared/ui/modals/simple-editor/NotePickerCombobox";
